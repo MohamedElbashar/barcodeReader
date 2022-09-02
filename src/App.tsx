00ee09f7b4b0332cc0de2b8bd0c9927f6fcc8f90
Mohamed Elbashar <mohamedbashar200@gmail.com>
2022-09-02 18:49:54 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import ImgDecode from "./components/ImgDecode/ImgDecode";
+
+function App() {
+  return (
+    <div className="App">
+      <ImgDecode></ImgDecode>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ImgDecode/ImgDecode.tsx
@@ -0,0 +1,59 @@
+import { BarcodeReader } from "dynamsoft-javascript-barcode";
+import { ChangeEvent, Component } from "react";
+import "./dbr";
+
+export default class ImgDecode extends Component {
+  pReader: Promise<BarcodeReader> = BarcodeReader.createInstance();
+
+  decodeImg = async (e: ChangeEvent<HTMLInputElement>) => {
+    try {
+      const reader = await this.pReader;
+      let results = await reader.decode(
+        "https://i.ibb.co/FVCmS6g/seq-10-camera-7.jpg"
+      );
+      for (let result of results) {
+        alert(result.barcodeText);
+      }
+      if (!results.length) {
+        alert("No barcode found");
+      }
+    } catch (ex: any) {
+      if (ex.message.indexOf("network connection error")) {
+        let customMsg =
+          "Failed to connect to Dynamsoft License Server: network connection error. Check your Internet connection or contact Dynamsoft Support (support@dynamsoft.com) to acquire an offline license.";
+        console.log(customMsg);
+        alert(customMsg);
+      }
+      throw ex;
+    }
+    e.target.value = "";
+  };
+
+  async componentWillUnmount() {
+    if (this.pReader) {
+      (await this.pReader).destroyContext();
+      console.log("ImgDecode Component Unmount");
+    }
+  }
+  async componentDidMount() {
+    try {
+      await BarcodeReader.loadWasm();
+    } catch (ex: any) {
+      if (ex.message.indexOf("network connection error")) {
+        let customMsg =
+          "Failed to connect to Dynamsoft License Server: network connection error. Check your Internet connection or contact Dynamsoft Support (support@dynamsoft.com) to acquire an offline license.";
+        console.log(customMsg);
+        alert(customMsg);
+      }
+      throw ex;
+    }
+  }
+
+  render() {
+    return (
+      <div className="ImgDecode">
+        <input type="file" onChange={this.decodeImg} />
+      </div>
+    );
+  }
+}
